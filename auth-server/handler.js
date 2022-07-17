a46f8f8ab0b55be62d7a972869c3f013749d83b9
FoliFF <olof@hisham.se>
2022-07-17 16:06:19 +0200

    Task 4.2 update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,6 +1,6 @@
 const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
-const calendar = google.calendar('v3');
+const calendar  = google.calendar('v3');
 
 /*
  * SCOPES allows you to set access levels; this is set to readonly for now because you don't have  
@@ -60,4 +60,40 @@ module.exports.getAuthURL = async () => {
       authUrl: authUrl,
     }),
   };
+};
+
+module.exports.getAccessToken = async (event) => {
+  // The values used to instantiate the OAuthClient are at the top of the file
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent('${event.pathParameters.code}');
+
+  return new Promise((resolve, reject) => {
+    /*
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+     */
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) { return reject(err);}
+      return resolve(token);
+    });
+  }).then((token) => {
+    // Respond with OAuth token
+    return{
+      statusCode: 200,
+      body: JSON.stringify(token),
+    };
+  }).catch((err) => {
+    // Handle Error
+    console.error(err);
+    return{
+      statusCode: 500,
+      body: JSON.stringify(err),
+    };
+  });
 };
