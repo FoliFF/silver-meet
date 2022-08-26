c081ab078e5cea988ce66e3df6de9c6642fe1913
FoliFF <olof@hisham.se>
2022-08-26 21:31:10 +0200

    Task 4.7 complete

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
     handleInputChanged = (event) => {
-        this.props.updateEvents(undefined, event.target.value);
-        this.setState({
-            renderNumber: event.target.value
-        });
+        let inputValue = event.target.value;
+        if (inputValue >= 33 || inputValue <= 0){
+            this.setState({
+                renderNumber: event.target.value,
+                infoText: 'Please enter a number 1 - 32.',
+            });
+        } else {
+            this.setState({
+                renderNumber: event.target.value,
+                infoText: '',
+            })
+        }
+        this.props.updateEvents(undefined, inputValue);
+        
     }
 
     constructor() {
         super();
         this.state = {
-            renderNumber: 32
+            renderNumber: 32,
+            infoText: '',
         }
     }
 
@@ -22,6 +34,7 @@ class NumberOfEvents extends Component {
             <div className="number-of-events">
                 <p className="input-label">Number of Events:</p>
                 <input id="render-number" type="number" className="render-number" value={renderNumber} onChange={this.handleInputChanged}></input>
+                <ErrorAlert text={this.state.infoText} />
             </div>
         );
     }
